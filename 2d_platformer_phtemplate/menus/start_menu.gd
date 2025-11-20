df98501f226e703fb667032abe4bb553c07bc9af
Maltel71 <156253099+Maltel71@users.noreply.github.com>
2025-11-20 12:27:59 +0100

    fixes

--- 2d_platformer_phtemplate/menus/start_menu.gd
+++ 2d_platformer_phtemplate/menus/start_menu.gd
@@ -1,13 +1,17 @@
 extends Control
 
 @export var game_scene: PackedScene
+@export var hover_sound: AudioStream
 
 @onready var start_button = $StartButton
 @onready var quit_button = $QuitButton
+@onready var audio_player = $AudioStreamPlayer
 
 func _ready():
 	start_button.pressed.connect(_on_start_pressed)
 	quit_button.pressed.connect(_on_quit_pressed)
+	start_button.mouse_entered.connect(_on_button_hover)
+	quit_button.mouse_entered.connect(_on_button_hover)
 
 func _on_start_pressed():
 	if game_scene:
@@ -17,3 +21,8 @@ func _on_start_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_button_hover():
+	if hover_sound and audio_player:
+		audio_player.stream = hover_sound
+		audio_player.play()
